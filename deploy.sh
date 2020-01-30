5caae826d4987932757c4191562f3e4bcbe7699e
Jonathan Winandy <jonathan@univalence.io>
2020-01-30 15:55:44 +0100

    ++

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "$PATH"
-
 clojure -A:pack mach.pack.alpha.skinny --no-libs --project-path speculoos.jar
 
-mvn deploy:deploy-file -Dfile=speculoos.jar -DrepositoryId=clojars -Durl=https://clojars.org/repo -DpomFile=pom.xml -e
+mvn -s ./.circleci/settings.xml deploy:deploy-file -Dfile=speculoos.jar -DrepositoryId=clojars -Durl=https://clojars.org/repo -DpomFile=pom.xml -e
